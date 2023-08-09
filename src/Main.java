92e539a7554918dbd902a8d0c7395608e0b97595
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-08-09 12:19:49 -0400

    Added Main class for initial application logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-package src;
-
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Test for the project");
+    public static void main(String[] args) {
+        Management management = new Management();
+
+        System.out.println("Test 1: ");
+        management.hireTest1();
+
+        management.hireTest2();
+
+        System.out.println("Test 3: ");
+        System.out.println("Employees description: ");
+        management.hireTest3();
     }
 }
